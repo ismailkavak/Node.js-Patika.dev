5d73b96bbdfaf02285db4d8ec2db79e7f343acdb
ismailkavak <poyrazkvk@hotmail.com>
2024-08-08 15:06:15 +0300

    Used template engine ejs module

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs')
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs")
+
+// MIDDLEWARE
+app.use(express.static('public'))
+
+// ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 4000;
