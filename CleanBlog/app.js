922cd4bf3076dcd3ef5633b5be4fabd6394fcb0c
ismailkavak <poyrazkvk@hotmail.com>
2024-08-23 22:30:43 +0300

    Added edit page router

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override')
 const app = express();
 const ejs = require('ejs');
-const Post = require('./models/Post')
+const Post = require('./models/Post');
+
 
 mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
@@ -14,24 +15,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', async (req, res) => {
-  const posts = await Post.find({})
+  const posts = await Post.find({});
   res.render('index', {
-    posts
+    posts,
   });
 });
 
 app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
+  const post = await Post.findById(req.params.id);
   res.render('post', {
-    post
+    post,
   });
 });
 
 app.get('/index', (req, res) => {
-  res.render('index');
+  res.redirect('/');
 });
 
 app.get('/about', (req, res) => {
@@ -47,9 +49,16 @@ app.get('/add_post', (req, res) => {
 });
 
 app.post('/posts', async (req, res) => {
-  await Post.create(req.body)
+  await Post.create(req.body);
   res.redirect('/');
 });
+
+app.get('/posts/edit/:id', async (req, res) => {
+  const post = await Post.findOne({ _id : req.params.id})
+  res.render('edit', {
+    post
+  })
+})
 
 const port = 4000;
 app.listen(port, () => {
